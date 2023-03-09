9e73fe7f8c8996d6e4cc4052b3704d2a8f26b1be
Craig - qrayg <craig@craigerskine.com>
2023-03-09 14:37:12 -0600

    ...

--- .eleventy.js
+++ .eleventy.js
@@ -8,8 +8,12 @@ const yaml = require("js-yaml");
 const CleanCSS = require("clean-css");
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy({'_site/_assets/img': '_assets/img'});
-  eleventyConfig.addPassthroughCopy({'_site/_assets/_root': './'});
+  eleventyConfig.setDataFileBaseName('_data');
+
+  eleventyConfig.addPassthroughCopy({
+    '_site/_assets/img': '_assets/img',
+    '_site/_assets/_root': './',
+  });
   
   eleventyConfig.addPlugin(EleventyRenderPlugin);
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
