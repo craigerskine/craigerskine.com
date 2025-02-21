5c6f199b846c6e2b8e7d8b0e4c179a03ed9be097
Craig @craigerskine @qrayg <craig@craigerskine.com>
2025-02-21 13:34:10 -0600

    better menu

--- _site/_app/_app.js
+++ _site/_app/_app.js
@@ -1,89 +1,89 @@
 // instant page
-import "instant.page";
+import 'instant.page';
 
 // icons
-import "iconify-icon";
+import 'iconify-icon';
 
 // color mode
 const toggleColorMode = function () {
   // light
-  if (document.documentElement.classList.contains("dark")) {
-    document.documentElement.classList.remove("dark");
-    localStorage.setItem("color-mode", "light");
+  if (document.documentElement.classList.contains('dark')) {
+    document.documentElement.classList.remove('dark');
+    localStorage.setItem('color-mode', 'light');
     return;
   }
   // dark
-  document.documentElement.classList.add("dark");
-  localStorage.setItem("color-mode", "dark");
+  document.documentElement.classList.add('dark');
+  localStorage.setItem('color-mode', 'dark');
 };
 
-document.querySelectorAll(".color-mode").forEach((btn) => {
-  btn.addEventListener("click", toggleColorMode);
+document.querySelectorAll('.color-mode').forEach((btn) => {
+  btn.addEventListener('click', toggleColorMode);
 });
 
 // twind
-import { install, injectGlobal, autoDarkColor } from "@twind/core";
-import presetAutoprefix from "@twind/preset-autoprefix";
-import presetTailwind from "@twind/preset-tailwind";
-import presetLineClamp from "@twind/preset-line-clamp";
+import { install, injectGlobal, autoDarkColor } from '@twind/core';
+import presetAutoprefix from '@twind/preset-autoprefix';
+import presetTailwind from '@twind/preset-tailwind';
+import presetLineClamp from '@twind/preset-line-clamp';
 
 install({
   presets: [presetAutoprefix(), presetTailwind(), presetLineClamp()],
-  darkMode: "class",
+  darkMode: 'class',
   darkColor: autoDarkColor,
   hash: false,
   theme: {
     extend: {
       screens: {
-        xs: "425px",
+        xs: '425px',
       },
       colors: ({ theme }) => ({
-        pri: theme("colors.orange"),
-        slate: { 950: "#020617" },
-        gray: { 950: "#030712" },
-        zinc: { 950: "#09090B" },
-        neutral: { 950: "#0A0A0A" },
-        stone: { 950: "#0C0A09" },
-        red: { 950: "#450A0A" },
-        orange: { 950: "#431407" },
-        amber: { 950: "#451A03" },
-        yellow: { 950: "#422006" },
-        lime: { 950: "#1A2E05" },
-        green: { 950: "#052E16" },
-        emerald: { 950: "#022C22" },
-        teal: { 950: "#042F2E" },
-        cyan: { 950: "#083344" },
-        sky: { 950: "#082F49" },
-        blue: { 950: "#172554" },
-        indigo: { 950: "#1E1B4B" },
-        violet: { 950: "#2E1065" },
-        purple: { 950: "#3B0764" },
-        fuchsia: { 950: "#4A044E" },
-        pink: { 950: "#500724" },
-        rose: { 950: "#4C0519" },
+        pri: theme('colors.orange'),
+        slate: { 950: '#020617' },
+        gray: { 950: '#030712' },
+        zinc: { 950: '#09090B' },
+        neutral: { 950: '#0A0A0A' },
+        stone: { 950: '#0C0A09' },
+        red: { 950: '#450A0A' },
+        orange: { 950: '#431407' },
+        amber: { 950: '#451A03' },
+        yellow: { 950: '#422006' },
+        lime: { 950: '#1A2E05' },
+        green: { 950: '#052E16' },
+        emerald: { 950: '#022C22' },
+        teal: { 950: '#042F2E' },
+        cyan: { 950: '#083344' },
+        sky: { 950: '#082F49' },
+        blue: { 950: '#172554' },
+        indigo: { 950: '#1E1B4B' },
+        violet: { 950: '#2E1065' },
+        purple: { 950: '#3B0764' },
+        fuchsia: { 950: '#4A044E' },
+        pink: { 950: '#500724' },
+        rose: { 950: '#4C0519' },
       }),
       fontFamily: ({ theme }) => ({
-        sans: ["Inter", ...theme("fontFamily.sans")],
+        sans: ['Inter', ...theme('fontFamily.sans')],
       }),
       keyframes: {
         blob: {
-          "15%": { transform: "scale(1.4, 1.2)" },
-          "40%": { transform: "scale(.9, .9)" },
-          "75%": { transform: "scale(1.08, 1)" },
-          "100%": { transform: "scale(1, 1)" },
+          '15%': { transform: 'scale(1.4, 1.2)' },
+          '40%': { transform: 'scale(.9, .9)' },
+          '75%': { transform: 'scale(1.08, 1)' },
+          '100%': { transform: 'scale(1, 1)' },
         },
       },
       animation: {
-        blob: "blob .3s ease-in-out",
+        blob: 'blob .3s ease-in-out',
       },
     },
   },
   rules: [
-    ["text-wrap-(unset|wrap|nowrap|balance)", "textWrap"],
-    ["btn-", ({ $$ }) => `bg-white/50 flex items-center justify-center relative rounded-full ring-(inset 1 black/10) shadow motion-safe:(transition) group-hover:(motion-safe:(animate-blob) after:(border-(t white/50) absolute inset-0 content-[''] rounded-full) bg-${$$}-500/50 bg-gradient-to-b from-white/30 via-transparent to-${$$}-500/30 text-${$$}-800 ring-${$$}-900/20 shadow-(lg ${$$}-500/30)) dark:(bg-black/20 ring-white/10)`],
-    ["tab-", ({ $$ }) => `flex items-center justify-center relative rounded-full ring-(1 gray-200 inset) shadow motion-safe:(transition) after:(border-(t white/50) absolute inset-0 content-[''] rounded-full) bg-${$$}-500/50 bg-gradient-to-b from-white/30 via-transparent to-${$$}-500/30 text-${$$}-800 ring-${$$}-900/20 shadow-(lg ${$$}-500/30)`],
-    ["bg-grid", { "background-image": "url(\"data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 6 6' width='6' height='6' fill='currentColor'><path d='m6 5v1h-1v-1zm-4 0v1h-1v-1zm3-1v1h-1v-1zm-2 0v1h-1v-1zm1-1v1h-1v-1zm-1-1v1h-1v-1zm2 0v1h-1v-1zm-3-1v1h-1v-1zm4 0v1h-1v-1zm-5-1v1h-1v-1z' /></svg>\")" }],
-    ["bg-paper", { "background-image": "url(/_assets/img/bg-paper.png)", "@apply": "bg-fixed" }],
+    ['text-wrap-(unset|wrap|nowrap|balance)', 'textWrap'],
+    ['btn-', ({ $$ }) => `bg-white/50 flex items-center justify-center relative rounded-full ring-(inset 1 black/10) shadow motion-safe:(transition) group-hover:(motion-safe:(animate-blob) after:(border-(t white/50) absolute inset-0 content-[''] rounded-full) bg-${$$}-500/50 bg-gradient-to-b from-white/30 via-transparent to-${$$}-500/30 text-${$$}-800 ring-${$$}-900/20 shadow-(lg ${$$}-500/30)) dark:(bg-black/20 ring-white/10)`],
+    ['tab-', ({ $$ }) => `flex items-center justify-center relative rounded-full ring-(1 gray-200 inset) shadow motion-safe:(transition) after:(border-(t white/50) absolute inset-0 content-[''] rounded-full) bg-${$$}-500/50 bg-gradient-to-b from-white/30 via-transparent to-${$$}-500/30 text-${$$}-800 ring-${$$}-900/20 shadow-(lg ${$$}-500/30)`],
+    ['bg-grid', { 'background-image': 'url("data:image/svg+xml,<svg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 6 6\' width=\'6\' height=\'6\' fill=\'currentColor\'><path d=\'m6 5v1h-1v-1zm-4 0v1h-1v-1zm3-1v1h-1v-1zm-2 0v1h-1v-1zm1-1v1h-1v-1zm-1-1v1h-1v-1zm2 0v1h-1v-1zm-3-1v1h-1v-1zm4 0v1h-1v-1zm-5-1v1h-1v-1z\' /></svg>")' }],
+    ['bg-paper', { 'background-image': 'url(/_assets/img/bg-paper.png)', '@apply': 'bg-fixed' }],
   ],
 });
 
@@ -107,8 +107,8 @@ injectGlobal`
 `;
 
 // alpinejs
-import Alpine from "alpinejs";
-import focus from "@alpinejs/focus";
+import Alpine from 'alpinejs';
+import focus from '@alpinejs/focus';
 Alpine.plugin(focus);
 window.Alpine = Alpine;
 Alpine.start();
